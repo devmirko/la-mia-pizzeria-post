9849ad4c83865db435c730ece5456ec9cbdadc49
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-11-22 13:41:39 +0100

    update

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -2,14 +2,22 @@
 using la_mia_pizzeria_razor_layout.Data;
 using Microsoft.Extensions.Hosting;
 using la_mia_pizzeria_razor_layout.Models;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace la_mia_pizzeria_razor_layout.Controllers
 {
     public class PizzaController : Controller
     {
+        PizzaDbContext db;
+
+        public PizzaController() : base()
+        {
+            db = new PizzaDbContext();
+        }
+
         public IActionResult Index()
         {
-            PizzaDbContext db = new PizzaDbContext();
+            //PizzaDbContext db = new PizzaDbContext();
 
             List<Pizza> listaPizza = db.Pizza.ToList();
 
@@ -19,16 +27,43 @@ namespace la_mia_pizzeria_razor_layout.Controllers
         public IActionResult Detail(int id)
         {
 
-            PizzaDbContext db = new PizzaDbContext();
+            //PizzaDbContext db = new PizzaDbContext();
 
             Pizza pizza = db.Pizza.Where(p => p.Id == id).FirstOrDefault();
 
             return View(pizza);
         }
 
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                
+                return View();
+            }
+
+            db.Pizza.Add(pizza);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+
+
+
+
 
     }
 
-    
+
 }
 
